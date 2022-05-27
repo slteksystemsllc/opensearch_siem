d13a5b0c83f278cd7141f041b5caaae6d32fd12f
Scott Lynch <62553406+slteksystemsllc@users.noreply.github.com>
2022-05-27 20:20:45 +0300

    Update automated_install.sh

--- automated_install.sh
+++ automated_install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 cd /opt/
 
-sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt install net-tools
 
-sudo apt-get install -y wget
+sudo apt update -y && sudo apt-get upgrade -y
 
-sudo wget https://github.com/slteksystemsllc/elasticsearch_siem/raw/main/scripts/prereq.sh && sudo bash prereq.sh
+sudo apt install -y wget
 
-sudo wget https://github.com/slteksystemsllc/elasticsearch_siem/raw/main/scripts/initialize.sh && sudo bash initialize.sh
+sudo wget https://github.com/slteksystemsllc/opensearch_siem/raw/main/scripts/prereq.sh && sudo bash prereq.sh
 
-cd /usr/share/elasticsearch/
+sudo wget https://github.com/slteksystemsllc/opensearch_siem/raw/main/scripts/initialize.sh && sudo bash initialize.sh
+
+cd /opt/opensearch_siem/
 
 docker-compose up --no-start
